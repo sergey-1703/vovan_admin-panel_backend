17d004ea53bc516f9b4b940a85ae7ac0899090a1
Sergey <161504480+sergey-1703@users.noreply.github.com>
2026-04-28 11:50:24 +0300

    root endpoint & security

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, redirect, url_for
 from config import get_admin_user, get_admin_password, get_secret_key
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, verify_jwt_in_request
 from db import set_is_banned
+import random
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = get_secret_key()
@@ -11,8 +12,10 @@ jwt = JWTManager(app)
 def login():
     username = request.form["username"]
     password = request.form["password"]
+    add_claims = {"rnd_int": random.randint(1_000, 1_000_000)}
     if username == get_admin_user() and password == get_admin_password():
-        return jsonify(access_token=create_access_token(identity="admin"))
+        return jsonify(access_token=create_access_token(identity="admin",
+                                                        additional_claims=add_claims))
     else:
         abort(401)
 
@@ -22,14 +25,19 @@ def set_status():
     user_id = request.form["user_id"]
     set_ban = request.form["set_ban"]
     try:
-        return {"result": set_is_banned(int(user_id), bool(set_ban))}
-    except Exception as e:
+        result = set_is_banned(user_id, set_ban)
+        return {"result": result}
+    except Exception:
         return {"result": "False" }
 
 
 @app.route("/", methods=["GET"])
 def root():
-    return "test"
+    try:
+        verify_jwt_in_request()
+        return redirect(url_for("panel"))
+    except Exception:
+        return redirect(url_for("login"))
 
 
 app.run(port=6000)
